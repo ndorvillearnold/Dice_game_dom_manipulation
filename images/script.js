43cae9739528f3385437aca183e1248522b35bc3
Naomy Arnold <NaomyA@newsmax.com>
2025-01-29 14:20:45 -0500

    added readme file and JS file

---
+++ images/script.js
@@ -0,0 +1,50 @@
+
+let diceCount = [ "diceOne", "diceTwo", "diceThree", "diceFour",  "diceFive", "diceSix"]
+
+
+let imageOne = document.getElementById("img1").innerHTML = "diceCount[0]"
+
+if(diceCount[0]){
+console.log("yo")
+
+
+}else{
+
+    console.log("NO")
+}
+
+
+
+function playerOne(number){
+    number = Math.random() * 6;
+    number = Math.floor(number);
+    console.log(number);
+   
+     return number;
+
+}
+
+playerOne()
+
+
+
+
+function playerTwo(number){
+ number = Math.random() * 6;
+ number = Math.floor(number);
+ console.log(number);
+
+return number;
+
+}
+playerTwo()
+
+
+while(playerOne){
+
+    if(){
+
+
+    }
+
+}
