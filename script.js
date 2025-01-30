9387704565f088889962b71a06128bc218321225
Naomy Arnold <NaomyA@newsmax.com>
2025-01-30 12:35:13 -0500

    added changes to JS file

--- script.js
+++ script.js
@@ -1,50 +1,43 @@
 
-let diceCount = [ "diceOne", "diceTwo", "diceThree", "diceFour",  "diceFive", "diceSix"]
+const imagePaths = [
+    './images/dice1.png',
+    './images/dice2.png',
+    './images/dice3.png',
+    './images/dice4.png',
+    './images/dice5.png',
+    './images/dice6.png'
+  ];
+
+  function randomNumber(){
+    return Math.floor(Math.random() * imagePaths.length);
+   }
+
+  const player1 =  document.getElementById("img1");
+  const player2 =  document.getElementById("img2");
+
+player1.setAttribute("src", imagePaths[randomNumber()]);
+player2.setAttribute("src", imagePaths[randomNumber()]);
+
+let draw = document.querySelector("h1").innerHTML = "DRAW!";
+let wins = document.querySelector("h1").innerHTML = "Wins!";
 
 
-let imageOne = document.getElementById("img1").innerHTML = "diceCount[0]"
-
-if(diceCount[0]){
-console.log("yo")
+console.log(player1);
+console.log(player2);
 
 
-}else{
-
-    console.log("NO")
-}
+if (player1 && player2){
 
 
-
-function playerOne(number){
-    number = Math.random() * 6;
-    number = Math.floor(number);
-    console.log(number);
+    console.log(draw)
+    
    
-     return number;
+   }else if(player1 > player2 ) {
+    console.log("Player 1 wins" + wins);
 
-}
+   }else{
+    ("Player 2 wins" + wins)
 
-playerOne()
-
-
-
-
-function playerTwo(number){
- number = Math.random() * 6;
- number = Math.floor(number);
- console.log(number);
-
-return number;
-
-}
-playerTwo()
+   }
 
 
-// while(playerOne){
-
-//     if(){
-
-
-//     }
-
-// }
